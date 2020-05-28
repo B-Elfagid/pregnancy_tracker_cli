924870ae9c968ad6312f5bd10c25cc8ce0f58290
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-28 01:14:40 +0000

    first commit

---
+++ lib/pregnancy_tracker_cli.rb
@@ -0,0 +1,6 @@
+require "pregnancy_tracker_cli/version"
+
+module PregnancyTrackerCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pregnancy_tracker_cli/version.rb
@@ -0,0 +1,3 @@
+module PregnancyTrackerCli
+  VERSION = "0.1.0"
+end
