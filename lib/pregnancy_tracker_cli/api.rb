80e3a359a18acebd8f17d0ea977e364e4fd5b116
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-29 00:21:48 +0000

    modified bin

--- lib/pregnancy_tracker_cli/api.rb
+++ lib/pregnancy_tracker_cli/api.rb
@@ -1,6 +1,10 @@
 class Api 
   
-  puts "Welcome to Baby Tracker"
+  BASE_URL = 'https://www.babycentre.co.uk/'
+  def self.get_pregnancy_tracker
+    res = RestClient.get(BASE_URL)
+  end
+  
   
 end 
   
